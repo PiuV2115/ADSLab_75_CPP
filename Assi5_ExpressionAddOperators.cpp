998ffba13adfd28a957a54c80446e4146d3513e5
Priya Kshirsagar <144653180+PiuV2115@users.noreply.github.com>
2025-08-07 08:42:05 +0530

    Update Assi5_ExpressionAddOperators.cpp

--- Assi5_ExpressionAddOperators.cpp
+++ Assi5_ExpressionAddOperators.cpp
@@ -1,3 +1,22 @@
+/*
+Problem Title:
+Expression Add Operators using Divide and Conquer
+
+Problem Statement:
+You are given a string num that contains only digits and an integer target.
+Your task is to add binary operators '+', '-', or '*' between the digits in num without reordering them, so that the resulting mathematical expression evaluates to the target value.
+
+Return all valid expressions (as strings) that evaluate to the target value.
+You can return them in any order.
+
+Constraints:
+The input string num will contain only digits (0–9) and will not be empty.
+Leading zeros are not allowed, except for the number zero itself (e.g., "05" is invalid).
+The target value can be positive or negative.
+Operators must be added between digits, and digits cannot be rearranged or removed.
+All evaluated expressions must be valid and follow standard operator precedence (*) has higher precedence than (+) or( -).
+*/
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -43,3 +62,12 @@ int main() {
 
     return 0;
 }
+/*
+Input
+num = "123"
+target = 6
+
+output
+["1+2+3", "1*2*3"]
+
+*/
