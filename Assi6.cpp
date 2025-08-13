4d6add60c97ffb15a9d47fe457de2b01328b9b8c
Priya Kshirsagar <144653180+PiuV2115@users.noreply.github.com>
2025-08-13 16:02:26 +0530

    Update Assi6_MaxShops.cpp

--- Assi6.cpp
+++ Assi6.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-int find(int s[10],int e[10],int n,int k)
+void findMax(int s[10],int e[10],int n,int k)
 {
 	int visit[10];
 	int person=0,count=0;
@@ -11,67 +11,88 @@ int find(int s[10],int e[10],int n,int k)
 	{
 		visit[i]=0;
 	}
-	while(person<k)
+	
+	while(person<k)	//outermost loop for k people
 	{
-	    int endtime=0;
-	    do
+	    int endtime=0;	//starting end time for each peson will be 0 
+	    
+	    do	//middle loop
 	    {
-	        count=0;
-	        int min=9999,idx=0;
-	       	for(int i=0;i<n;i++)
+	        count=0; //to terminate do-while loop
+	        
+	        int min=9999,idx=0;   //to find Minimum
+	        
+	        for(int i=0;i<n;i++)	//innermost loop
 	    	{
-	    	    if(endtime<=s[i]&&visit[i]!=1)
-	    	    {
-	    	        count++;
-		    	if(min>s[i])
-			    {
+	    	    	if(endtime<=s[i]&&visit[i]!=1)	//condition to check constraints
+	    	    	{
+	    	       	count++;
+	    	       	
+		    		if(min>s[i])	// to check minimum starting time
+			   	{
 					idx=i;
 					min=s[i];
 				}
 				else
 				{
-			        continue;
+			        	continue;
 				}
-	    	    }
-			}
-			if(count>0)
-			{
+	    	    	}  //end of outer if
+		
+		}	//end of innermost for loop
+	
+		if(count>0)	//condition to update
+		{
 			 visit[idx]=1;
-		     result++;
-	         endtime=e[idx];
-			}
-			else
-			{
-			    break;
-			}
-	    	}while(1);
-	    	 person++;
-	    }		   
-	   	return result;
-	}
+		    	 result++;
+	              	 endtime=e[idx];
+		}
+		else
+		{
+			 break; //else break do-while loop
+		}
+		
+	   }while(1); //end of do-while
+	    	
+	   person++; //increamenting 
+	   
+	}//end of outermost while loop
+			   
+cout<<"Maximum no. of shops Visited by Peoples : "<<result<<"\n"; //returning maximum shops
+
+}//end of find function
 
 
 
 
 int main()
 {
-	int s[10],e[10],n,k,result;
+	int s[10],e[10],n,k,result=0;
 	cout<<"Enter no. of Shops : ";
 	cin>>n;
 	
 	cout<<"Enter no. of Peoples :";
 	cin>>k;
 	
-	cout<<"Enter Starting Times of shops : ";
+	cout<<"Enter Starting Times of shops : \n";
 	for(int i=0;i<n;i++)
+	{
+		cout<<i+1<<"] ";
 		cin>>s[i];
+		cout<<endl;
+	}
 		
-	cout<<"Enter Ending Times of shops : ";
+		
+	cout<<"Enter Ending Times of shops : \n";
 	for(int i=0;i<n;i++)
+	{
+		cout<<i+1<<"] ";
 		cin>>e[i];
+		cout<<endl;
+	}
+		
+	findMax(s,e,n,k);
 	
-	result=find(s,e,n,k);
-	
-	return result;
+	return 0;
 	
 }
